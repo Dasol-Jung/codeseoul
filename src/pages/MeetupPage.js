1b04d19b9ffada78d5a06b08465e4d28fde4f4fd
Dasol-Jung <xenosophian@gmail.com>
2018-08-12 09:43:03 +0900

    fetch api data

--- src/pages/MeetupPage.js
+++ src/pages/MeetupPage.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React from "react";
 
-const MeetupPage = () => (
-  <div>Meetup page</div>
-);
+class MeetupPage extends React.Component {
+  state = {
+    data: []
+  };
+
+  componentDidMount() {
+    fetch("https://api.meetup.com/Learn-Teach-Code-Seoul/events")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          data: data
+        });
+      });
+  }
+
+  render() {
+    console.log(this.state.data)
+    const events = this.state.data.map(event => <div key={event.id}>{event.duration}</div>);
+
+    return <div>{events}</div>;
+  }
+}
 
 export default MeetupPage;
